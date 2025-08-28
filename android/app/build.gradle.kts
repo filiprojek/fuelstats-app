5583c77a380ca88b1e4c9a81c5e574ebc1bb1eb4
Filip Rojek <filip@filiprojek.cz>
2025-08-28 22:06:28 +0200

    Renamed the package name to cz.filiprojek.fuelstats

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.android_fuelstats"
+    namespace = "cz.filiprojek.fuelstats"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.android_fuelstats"
+        applicationId = "cz.filiprojek.fuelstats"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
