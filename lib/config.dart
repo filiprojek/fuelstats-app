66f158512e62356dc59ebf1d2dd9350a110af4c3
Filip Rojek <filip@filiprojek.cz>
2025-08-28 15:26:35 +0200

    fix(config.dart): update default API base URL to point to the new server address for development purposes

--- lib/config.dart
+++ lib/config.dart
@@ -1,2 +1,2 @@
 const String apiBaseUrl =
-    String.fromEnvironment('API_BASE_URL', defaultValue: 'http://localhost:3000');
+    String.fromEnvironment('API_BASE_URL', defaultValue: 'http://10.123.0.102:6060');
