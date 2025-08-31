bd7259153fc39c511fba5bfced000bbd5246c1ee
Filip Rojek <filip@filiprojek.cz>
2025-08-31 23:10:49 +0200

    Adjust auth screen logo placement

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -71,25 +71,27 @@ class _LoginScreenState extends State<LoginScreen> {
         padding: const EdgeInsets.all(24.0),
         child: Form(
           key: _formKey,
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 40),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
                     'assets/icon/app_icon.png',
                     width: 100,
                     height: 100,
                   ),
-                  const SizedBox(height: 16),
-                  Text(
-                    'Log in to Fuel Stats',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Log in to Fuel Stats',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  const SizedBox(height: 32),
+                ),
+                const SizedBox(height: 32),
                   TextFormField(
                     controller: _emailController,
                     decoration: InputDecoration(
